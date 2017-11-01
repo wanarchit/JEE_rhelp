120cebb94031a74de3c477f485c41be5d8ac516b
Paul <Paul@192.168.1.95>
2017-11-01 11:04:52 +0100

    reservations

---
+++ RHelp/src/java/converters/voitureConverter.java
@@ -0,0 +1,25 @@
+package converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+
+/**
+ *
+ * @author Paul
+ */
+
+public class voitureConverter implements Converter{
+
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent ui, String value) {
+        return null;
+    }
+
+    public String getAsString(FacesContext fc, UIComponent ui, Object value) {
+        return null;
+    }
+
+ }
