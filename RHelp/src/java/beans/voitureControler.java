6f134efa007244044692977eed1478d5d9e4d5e1
Delphine Rousse <delphine1237@hotmail.fr>
2017-11-22 09:11:56 +0100

    Le graphisme !! Les boutons :D

--- RHelp/src/java/beans/voitureControler.java
+++ RHelp/src/java/beans/voitureControler.java
@@ -25,44 +25,44 @@ import javax.faces.view.ViewScoped;
  */
 @Named(value = "voitureControler")
 @ViewScoped
-public class voitureControler implements Serializable{
+public class voitureControler implements Serializable {
 
     @EJB
     private voitureDAO dao;
     private Voiture selectedVoi;
     private List<Voiture> listVoiture;
-    
+
     /**
      * Creates a new instance of voitureControler
      */
     public voitureControler() {
-        
+
     }
-    
+
     public List<Voiture> getVoitures() {
         return this.listVoiture;
     }
-    
+
     @PostConstruct
     public void init() {
         this.listVoiture = dao.getAll();
     }
-    
-    public void addVoi(Voiture voi){
+
+    public void addVoi(Voiture voi) {
         voi.setDisponibilite(true);
         dao.saveVoiture(voi);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Le véhicule "+voi.getMarque()+" "+voi.getModele()+" à bien été ajouté.", ""));
+                "Le véhicule " + voi.getMarque() + " " + voi.getModele() + " à bien été ajouté.", ""));
     }
-    
-    public void removeVoi(String idV){
+
+    public void removeVoi(String idV) {
         dao.remove(idV);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Le véhicule "+selectedVoi.getMarque()+" "+selectedVoi.getModele()+" à bien été supprimé.", ""));
+                "Le véhicule " + selectedVoi.getMarque() + " " + selectedVoi.getModele() + " à bien été supprimé.", ""));
     }
-    
+
     public Voiture getSelectedVoi() {
-        if (selectedVoi == null){
+        if (selectedVoi == null) {
             selectedVoi = getVoitures().get(0);
         }
         return selectedVoi;
@@ -71,32 +71,41 @@ public class voitureControler implements Serializable{
     public void setSelectedVoi(Voiture selectedVoi) {
         this.selectedVoi = selectedVoi;
     }
-    
-    public void updateVoi(Voiture voi){
+
+    public void updateVoi(Voiture voi) {
         dao.updateVoi(voi);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Les informations de la voiture "+selectedVoi.getMarque()+" "+selectedVoi.getModele()+" ont bien été mises à jour.", ""));
+                "Les informations de la voiture " + selectedVoi.getMarque() + " " + selectedVoi.getModele() + " ont bien été mises à jour.", ""));
     }
-    
-    public Voiture getOneVoiture(String pla){
+
+    public Voiture getOneVoiture(String pla) {
         return dao.getOneVoiture(pla);
     }
-    
-    public ArrayList<Voiture> getVoitureRevis(){
+
+    public ArrayList<Voiture> getVoitureRevis() {
         ArrayList<Voiture> voitRevis = new ArrayList();
         List<Voiture> listvoit = getVoitures();
-        
+
         Date currentDate = new Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(currentDate);
-        cal.add(Calendar.DATE, -15);
+        cal.add(Calendar.DATE, +15);
         Date dateFinale = cal.getTime();
-                
+
         for (Voiture voi : listvoit) {
-            if (voi.getDateRevision().after(dateFinale)) {
-               voitRevis.add(voi);
+            if (voi.getDateRevision().before(dateFinale)) {
+                voitRevis.add(voi);
             }
         }
         return voitRevis;
+    }
+
+    public String getAVAPRevis(Voiture voit) {
+
+        Date currentDate = new Date();
+        if (voit.getDateRevision().before(currentDate)) {
+            return "<font color = '#A90024'>(Attention, la date est déjà passée)</font>";
+        }
+        else return "(Date à venir dans moins de 15 jours)";
     }
 }
